18fbd2e000089a5ef0b444233ae305676dbf9fc4
danyilmaz <dan.yilmaz@growthstreet.co.uk>
2017-02-15 22:50:37 +0000

    added docstrings, removed default values from deleted_at/complted_at fields

--- origin/tasks/forms.py
+++ origin/tasks/forms.py
@@ -3,6 +3,11 @@ from tasks.models import Task
 
 
 class CreateTaskForm(ModelForm):
+    """Custom create task form
+
+    This form takes a user object when it is initialised.
+    On form.save, we write that user to the  'created_by' field
+    """
     def __init__(self, *args, **kwargs):
         self.user = kwargs.pop('user')
         super().__init__(*args, **kwargs)
--- origin/tasks/views.py
+++ origin/tasks/views.py
@@ -9,6 +9,10 @@ from django.utils import timezone
 
 
 class ListTasksView(ListView):
+    """List view for all tasks.
+    we always hide 'deleted' tasks.
+    optionally hide completed tasks.
+    """
     model = Task
     template_name = 'task_list.html'
     paginate_by = 30
@@ -22,28 +26,42 @@ class ListTasksView(ListView):
 
 
 class CreateTaskView(CreateView):
+    """View for creating new tasks.
+    """
     form_class = CreateTaskForm
     template_name = 'create_task.html'
     success_url = reverse_lazy('tasks:task_list')
 
     def get_form_kwargs(self):
+        """Pass the logged in user to the form. This is safer than doing something
+        nasty with hidden fields.
+        """
         form_kwargs = super().get_form_kwargs()
         form_kwargs['user'] = self.request.user
         return form_kwargs
 
 
 class EditTaskView(UpdateView):
+    """View to allow editing of tasks.
+    """
     model = Task
     template_name = 'edit_task.html'
     success_url = reverse_lazy('tasks:task_list')
     fields = ['name', 'description']
 
     def get_object(self):
+        """We override get object here so we can pass it the logged in user.
+        This ensures that users can only edit tasks they have created.
+        """
         obj = get_object_or_404(Task, pk=self.kwargs['pk'], created_by=self.request.user)
         return obj
 
 
 class BaseStatusToggleView(RedirectView):
+    """Base view which updates a task's status.
+    Both marking as Done and makring as Not Done share a lot of similar code
+    This is DRYer
+    """
     success_message = None
 
     def get_redirect_url(self):
@@ -58,6 +76,8 @@ class BaseStatusToggleView(RedirectView):
 
 
 class MarkDoneStatusView(BaseStatusToggleView):
+    """Child view to mark a task as done.
+    """
     success_message = 'Task marked as Done'
 
     def update_task(self):
@@ -67,6 +87,8 @@ class MarkDoneStatusView(BaseStatusToggleView):
 
 
 class MarkNotDoneStatusView(BaseStatusToggleView):
+    """Child view to mark a task as Todo
+    """
     success_message = 'Task marked as To Do'
 
     def update_task(self):
@@ -76,11 +98,18 @@ class MarkNotDoneStatusView(BaseStatusToggleView):
 
 
 class DeleteTaskView(DeleteView):
+    """Delete view to mark a task as deleted.
+    """
     model = Task
     success_url = reverse_lazy('tasks:task_list')
     template_name = 'delete_lender.html'
 
     def delete(self, request, *args, **kwargs):
+        """Override this method, as we want to persist the object in the db for audit reasons.
+
+        get_object_or_404 allows us to be confident that users may only delete tasks
+        they themselves have created
+        """
         self.object = get_object_or_404(Task, pk=self.kwargs['pk'], created_by=self.request.user)
         success_url = self.get_success_url()
         self.object.delete(user=request.user)
